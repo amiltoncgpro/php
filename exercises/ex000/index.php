5ac4ba6424004d21bc95b3da38df6ae40a0cc9b9
Amilton Gove <amiltoncpro@gmail.com>
2024-05-29 10:58:17 +0200

    Create index.php

---
+++ exercises/ex000/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldição</title>
+</head>
+<body>
+    <h1>
+        <?php
+            //Os dois funcionam da mesma forma
+            //print "Olá Mundo!";
+            //Exemplo de caracteres/codigo unicode: \u{1F30E}
+            echo "Olá Mundo! \u{1F30E}";
+        ?>     
+    </h1>
+    <p>Vamos tentar os livrar de maldição.</p>
+</body>
+</html>
